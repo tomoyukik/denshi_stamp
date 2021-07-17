5a40137786b12401b5c52e2989fe56b9c6e3d0b1
tomoyukik <kobori.tomoyuki@gmail.com>
2021-07-18 01:06:35 +0900

    update: stamp created
    - trim characters
    - circle strings with minimum enclosing window

--- stamp.py
+++ stamp.py
@@ -1,6 +1,9 @@
 # %%
 from PIL import Image, ImageDraw, ImageFont
 import matplotlib.font_manager as fm
+from matplotlib.pyplot import draw
+import numpy as np
+import cv2
 
 edge = 320
 font = fm.fontManager.ttflist[125]
@@ -17,12 +20,14 @@ class Stamp:
 
     def _generate_stamp(self):
         image = self._string_to_image(self.contents, self.font)
-        return self._circle(image)
+        image = self._circle(image)
+        return image
 
     def _character_to_image(self, character, font):
         image = Image.new(RGBA, (font.size, font.size), TRANSPARENT)
         draw = ImageDraw.Draw(image)
         draw.text((0, 0), character, font=font, fill='red')
+        image = self._crop(image)
         return image
     
     def _concat_chars(self, chars):
@@ -31,8 +36,8 @@ class Stamp:
         image = Image.new(RGBA, (width, height), TRANSPARENT)
         x, y = 0, 0
         for char in chars:
+            x = (width - char.width) // 2
             image.paste(char, (x, y))
-            x = 0
             y = char.height
         return image
     
@@ -42,18 +47,41 @@ class Stamp:
             char_image = self._character_to_image(char, font)
             char_images.append(char_image)
         string_image = self._concat_chars(char_images)
+        string_image = self._crop(string_image)
         return string_image
     
     def _circle(self, image):
         # rescale
         edge = min(image.height, image.width)
         rescaled = image.resize((edge, edge))
+        rescaled = self._crop(rescaled)
         # circle
-        draw = ImageDraw.Draw(rescaled)
-        draw.ellipse((edge, edge, edge, edge), outline='red', width=6)
-        draw.ellipse((0, 0, edge, edge), outline='red', width=6)
-        trimmed = self._trim_circle(rescaled)
+        enclosed = self._enclose(rescaled)
+
+        trimmed = self._trim_circle(enclosed)
         return trimmed
+
+    #
+    def _enclosing_circle(self, image):
+        array = np.array(image, dtype=np.uint8)
+        array = cv2.cvtColor(array, cv2.COLOR_RGBA2GRAY)
+        retval, array = cv2.threshold(array, array.min() + 1, 255, cv2.THRESH_BINARY)
+        x0 = image.width // 2
+        y0 = image.height // 2
+        array = cv2.circle(array, (x0, y0), y0, 255, thickness=-1)
+        contours, hierarchy = cv2.findContours(array, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        (x1, y1), radius = cv2.minEnclosingCircle(contours[0])
+        return (x1, y1), radius
+        return (x0, y1), radius
+    #
+    def _enclose(self, image):
+        (x, y), radius = self._enclosing_circle(image)
+        diameter = int(radius * 2)
+        stamp = Image.new(RGBA, (diameter, diameter), TRANSPARENT)
+        stamp.paste(image, ((diameter - image.width) // 2, int(radius - y)))
+        draw = ImageDraw.Draw(stamp)
+        draw.ellipse((0, 0, diameter, diameter), outline='red', width=6)
+        return stamp
     
     def _trim_circle(self, image):
         # generate mask
@@ -68,6 +96,20 @@ class Stamp:
         # 合成
         return Image.composite(image, transparent, disk_mask)
 
+    def _object_range(self, bin_image, axis):
+        histogram = np.any(bin_image < bin_image.max(), axis=axis)
+        indices = np.where(histogram)[0]
+        x_min, x_max = min(indices), max(indices)
+        return x_min, x_max
+
+    def _crop(self, image):
+        array = np.array(image, dtype=np.uint8)
+        array = cv2.cvtColor(array, cv2.COLOR_RGBA2GRAY)
+        thres = (array.max() - array.min()) // 2
+        retval, array = cv2.threshold(array, thres, 255, cv2.THRESH_BINARY)
+        x_min, x_max = self._object_range(array, 0)
+        y_min, y_max = self._object_range(array, 1)
+        return image.crop((x_min - 1, y_min - 1, x_max + 1, y_max + 1))
+
 stmp = Stamp('小堀', edge, font)
 stmp.image
-# %%
