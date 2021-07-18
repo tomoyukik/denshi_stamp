75897ae157b5da7e73f4d3b4fab4ab8091a89470
tomoyukik <kobori.tomoyuki@gmail.com>
2021-07-18 21:50:37 +0900

    add: setup.py
    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='denshi_stamp',
+    version="0.0.1",
+    description="denshi stamp creator",
+    long_description="",
+    author='tomoyukikobori',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ],
+    packages=['stamp']
+)
